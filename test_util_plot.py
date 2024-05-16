c8cb3d2543f70a112a1004824991ab7fa04aaf26
Qing-kk <qing001124@gmail.com>
2024-05-16 17:41:08 -0500

    Merge with main

--- test_util_plot.py
+++ test_util_plot.py
@@ -6,8 +6,14 @@ Date: 9/11/2022
 
 import numpy as np
 from StarV.set.probstar import ProbStar
+<<<<<<< HEAD
 from StarV.util.plot import  plot_probstar
 # from StarV.util.plot import probstar2polytope
+=======
+from StarV.set.star import Star
+from StarV.util.plot import plot_probstar, plot_star, plot_2D_UnsafeSpec, get_bounding_box
+
+>>>>>>> d6f456f630416bad2b82975fff47be17aa363e64
 
 
 class Test(object):
@@ -70,6 +76,34 @@ class Test(object):
         else:
             print("Test Successfull!")
 
+    def test_plot_2D_UnsafeSpec(self):
+        
+         # 0.5 <= d_k <= 2.5 AND 0.2 <= v_k <= v_ub
+        unsafe_mat = np.array([[1.0, 0.0], [-1., 0.], [0., 1.], [0., -1.]])
+        unsafe_vec = np.array([2.5, -0.5, 90.5, -0.2])
+
+        try:
+            plot_2D_UnsafeSpec(unsafe_mat, unsafe_vec)
+        except Exception:
+            print('Test Fail!')
+            self.n_fails = self.n_fails + 1
+        else:
+            print("Test Successfull!")
+
+    def test_get_bounding_box(self):
+        
+         # 0.5 <= d_k <= 2.5 AND 0.2 <= v_k <= v_ub
+        unsafe_mat = np.array([[1.0, 0.0], [-1., 0.], [0., 1.], [0., -1.]])
+        unsafe_vec = np.array([2.5, -0.5, 90.5, -0.2])
+
+        try:
+            lb, ub = get_bounding_box(unsafe_mat, unsafe_vec)
+            print('lb = {}, ub = {}'.format(lb, ub))
+        except Exception:
+            print('Test Fail!')
+            self.n_fails = self.n_fails + 1
+        else:
+            print("Test Successfull!")
 
 
 if __name__ == "__main__":
@@ -80,7 +114,13 @@ if __name__ == "__main__":
     ================================\
     ===============================\n')
     # test_plot.test_probstar2polytope()
+<<<<<<< HEAD
     test_plot.test_plot_probstar()
+=======
+    # test_plot.test_plot_probstar()
+    test_plot.test_plot_2D_UnsafeSpec()
+    #test_plot.test_get_bounding_box()
+>>>>>>> d6f456f630416bad2b82975fff47be17aa363e64
     
     print('\n========================\
     =================================\
